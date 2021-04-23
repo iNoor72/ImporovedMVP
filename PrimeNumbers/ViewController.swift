f6d14557df35bd0038bdf841616293a6e34c427a
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-23 13:34:38 +0200

    Initial Commit

---
+++ PrimeNumbers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeNumbers
+//
+//  Created by Noor Walid on 23/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
